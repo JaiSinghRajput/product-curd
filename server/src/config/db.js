04cee92889afc754601129286e2c8f62ad9a7a78
Jai Singh Rajput <jaisinghrajput9461@gmail.com>
2026-04-24 12:08:01 +0530

    db updated

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import env from "./env.js";
+const DB_Name = "productr";
 
 const connectDb = async () => {
   if (!env.dbUrl) {
     throw new Error("DB_URL is required");
   }
 
-  await mongoose.connect(env.dbUrl, {
+  await mongoose.connect(`${env.dbUrl}/${DB_Name}`, {
     autoIndex: true,
   });
 };
